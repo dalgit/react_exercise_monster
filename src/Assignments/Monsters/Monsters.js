501bc06a56ede5d485efc4ddbeecc7562abc64eb
junki <junki646@gmail.com>
2022-08-12 11:56:43 +0900

    카드들 출력 구현, 검색 기능 구현

--- src/Assignments/Monsters/Monsters.js
+++ src/Assignments/Monsters/Monsters.js
@@ -3,42 +3,33 @@ import SearchBox from "./Components/SearchBox/SearchBox";
 import CardList from "./Components/CardList/CardList";
 import "./Monsters.scss";
 
-
-
-/**********************************************************
-  2. SearchBox 컴포넌트에 정의한 handleChange 메소드를 넘겨주고, 
-     호출 시 인자로 들어오는 이벤트객체(e)를 활용해 userInput 으로 setState.
-
-  3. 필터링 로직 구현 (filter 메소드 활용)
-      여기서 비교 대상은 monster 객체의 name 값입니다.
-      소문자로 바꾼 monster.name 값과 userInput값을 비교.
-      filter 메소드가 반환하는 값을 변수에 저장 후 return 문 안에 CardList에 props로 전달
-***********************************************************/
-
 function Monsters() {
   const [monsters, setMonsters] = useState([]);
   const [userInput, setUserInput] = useState("");
 
-  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'GET'
+    })
+      .then(res => res.json())
+      .then(setMonsters)},
+    []);
 
-useEffect(() => {
-  fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'GET'
-  })
-    .then(res => res.json())
-    .then(data => {
-      setData(data);
-    });
-},[]);
+  function handleChange(e){
+    setUserInput(e.target.value)
+  }
 
-  // SearchBox 에 props로 넘겨줄 handleChange 메소드 정의
+  function nameFilter(){
+    return monsters.filter((monster)=>(monster['name'].toLowerCase().includes(userInput)))
+  }
+
 
   return (
     <>
       <div className="monsters">
         <h1>컴포넌트 재사용 연습!</h1>
-        {/* <SearchBox handleChange=정의한메소드 /> */}
-        <CardList data={data}/>
+        <SearchBox handleChange={handleChange} />
+        <CardList monster={nameFilter()}/>
       </div>
     </>
   );
